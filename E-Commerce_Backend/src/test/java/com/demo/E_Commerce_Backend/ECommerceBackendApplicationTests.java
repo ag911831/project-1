89425eb320e612952b821229ec1e7e8e706412c2
ashish.gajraj <ag911831@student.nitw.ac.in>
2026-02-12 12:37:36 +0530

    Add project related things

---
+++ E-Commerce_Backend/src/test/java/com/demo/E_Commerce_Backend/ECommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.E_Commerce_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
